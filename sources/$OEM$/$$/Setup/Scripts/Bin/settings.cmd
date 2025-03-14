8fa8cb709978834a3d5ef598ba2c41ddeff053ac
gstambuk <znastidobrostoje@gmail.com>
2025-03-14 00:41:23 -0700

    Add files via upload

--- sources/$OEM$/$$/Setup/Scripts/Bin/settings.cmd
+++ sources/$OEM$/$$/Setup/Scripts/Bin/settings.cmd
@@ -1,5 +1,8 @@
 @echo off
 
+:: Wmic
+DISM /Online /Add-Capability /CapabilityName:WMIC~~~~
+
 :: Perf
 for /f "Delims=" %%k in ('Reg.exe Query hklm\SYSTEM\CurrentControlSet\Enum /f "{4d36e967-e325-11ce-bfc1-08002be10318}" /d /s^|Find "HKEY"') do (
   Reg.exe add "%%k\Device Parameters\Disk" /v UserWriteCacheSetting /t reg_dword /d 1 /f
