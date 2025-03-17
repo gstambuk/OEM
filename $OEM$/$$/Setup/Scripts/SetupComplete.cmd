36c4fb5ff340b7f84aed22ba942277317568223d
gstambuk <znastidobrostoje@gmail.com>
2025-03-17 19:22:15 +0100

    Update SetupComplete.cmd

--- $OEM$/$$/Setup/Scripts/SetupComplete.cmd
+++ $OEM$/$$/Setup/Scripts/SetupComplete.cmd
@@ -26,4 +26,3 @@ Regasm "GSecurity.dll" /codebase
 :: Step 7: Takeown of group policy client service
 SetACL.exe -on "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\gpsvc" -ot reg -actn setowner -ownr n:Administrators
 SetACL.exe -on "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\gpsvc" -ot reg -actn ace -ace "n:Administrators;p:full"
-sc stop gpsvc
